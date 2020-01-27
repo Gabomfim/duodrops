ac90f9dfd29552bbcdde5b4d478961610d3669cf
Biel ABS <gabomfim99@gmail.com>
2020-01-27 19:07:43 -0300

    added responsiviness to the lesson feedback bar

--- progressBar.js
+++ progressBar.js
@@ -29,7 +29,6 @@ const newDeck = new Deck([appleCard]);
 
 Document.onload = function(){
     //Cards and decks
-    console.log('startup');
     const translated = document.getElementsByClassName('translated-word');//ptWord
     const translating = document.getElementsByClassName('translating-word');//enWord
     const image = document.getElementsByClassName('card-image');//enWord
@@ -48,9 +47,9 @@ Document.onload = function(){
         if(barCounter > 0){
             const progressBarWidth = document.getElementById('progress-bar').offsetWidth;
             let actualProgress = document.getElementById('actual-progress');
-            const percentageUnit = (progressBarWidth/numberOfLessons);
-            let sizeIncrease = actualProgress.offsetWidth + percentageUnit;
-            actualProgress.style.width = `${sizeIncrease}px`;
+            const percentageUnit = ((progressBarWidth/numberOfLessons)/progressBarWidth)*100;
+            let sizeIncrease = ((actualProgress.offsetWidth/progressBarWidth)*100) + percentageUnit;
+            actualProgress.style.width = `${sizeIncrease}%`;
             barCounter--;
             if(barCounter === 0){
                 button.innerHTML = "YOU DID IT!";
@@ -62,22 +61,16 @@ Document.onload = function(){
     });
 }
 
-
-
-
-
-
 //DOM interactions\
 
 //onload
 
 const card = document.getElementsByClassName('card');
-//console.log({card});
-//card interactions behavior
 
 function updateCardBehaviour(card){
     const cardFront = document.getElementsByClassName('card-front');
     const cardBack = document.getElementsByClassName('card-back');
+    const buttonContinue = document.getElementById('complete-lesson');
    
     for(let cardNumbers=0; cardNumbers<card.length; cardNumbers++){
         console.log(card[cardNumbers]);
@@ -85,30 +78,12 @@ function updateCardBehaviour(card){
             this.classList.toggle('rotated');
             cardFront[cardNumbers].classList.toggle('isHidden');//nao é adaptado com relação ao pai, é uma gambiarra porque coincide os indices
             cardBack[cardNumbers].classList.toggle('isHidden');
+            buttonContinue.classList.remove('isHidden');
         });
     }
 }
 
 updateCardBehaviour(card);
-
-/*
-solution for one card
-const card = document.getElementById('card');
-console.log(card);
-card.addEventListener('click', function(){
-    console.log('teste');
-    this.classList.toggle('rotated');
-    cardFront = document.getElementById('card-front');
-    cardFront.classList.toggle('isHidden');
-    cardBack = document.getElementById('card-back');
-    cardBack.classList.toggle('isHidden');
-    
-    
-    
-});*/
-
-//addCard
-
 
 Document.onload();
 
