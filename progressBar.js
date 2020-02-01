5e3347005f3e91f5156313bac83e548cfa900c51
Biel ABS <gabomfim99@gmail.com>
2020-02-01 12:29:49 -0300

    comportamento de renderização concluído

--- progressBar.js
+++ progressBar.js
@@ -53,6 +53,10 @@ const starfruitCard = new Card(starfruit, 'https://source.unsplash.com/150x150/?
 const grapeCard = new Card(grape, 'https://source.unsplash.com/150x150/?grape', system);
 const newDeck = new Deck([appleCard, starfruitCard, grapeCard]);
 const fruitsLesson = new Lesson('fruits', newDeck);
+let exerciseIndex = 1;
+let barSize = fruitsLesson.exercises.deckCards.length;
+let barCounter = barSize;
+let button = document.getElementById('complete-lesson');
 
 function updateCardBehaviour(card){
     const cardFront = document.getElementsByClassName('card-front');
@@ -63,8 +67,9 @@ function updateCardBehaviour(card){
         console.log(card[cardNumbers]);
         card[cardNumbers].addEventListener('click', function(){
             this.classList.toggle('rotated');
-            cardFront[cardNumbers].classList.toggle('isHidden');//nao é adaptado com relação ao pai, é uma gambiarra porque coincide os indices
             cardBack[cardNumbers].classList.toggle('isHidden');
+            cardFront[cardNumbers].classList.toggle('isHidden');
+            
             buttonContinue.classList.remove('isHidden');
         });
     }
@@ -72,8 +77,6 @@ function updateCardBehaviour(card){
 
 progressBarBehaviour = () => {
     
-    let barSize = fruitsLesson.exercises.deckCards.length;
-    let barCounter = barSize;
     if(barCounter > 0){
             
         const progressBarWidth = document.getElementById('progress-bar').offsetWidth;
@@ -82,7 +85,10 @@ progressBarBehaviour = () => {
         let sizeIncrease = ((actualProgress.offsetWidth/progressBarWidth)*100) + percentageUnit;
         actualProgress.style.width = `${sizeIncrease}%`;
         barCounter--;
+        console.log("barCounter " + barCounter);
         if(barCounter === 0){
+            console.log('jooj');
+            button.classList.remove('isHidden');
             button.innerHTML = "YOU DID IT!";
             button.style.backgroundColor = "#F9D649";
         }
@@ -90,26 +96,31 @@ progressBarBehaviour = () => {
     }
 };
 
-renderNewExercise = (lesson, i) => {
+renderNewExercise = () => {
+    console.log(exerciseIndex);
     exerciseContainer = document.getElementById('exercise-container');
-    console.log(fruitsLesson.exercises.deckCards[i].translatingWord);
     exerciseContainer.innerHTML =
     `<div class="card card-shadow">
         <div class="card-front">
             <img class="card-image" width=150 height=150 />
-            <p class="translating-word">${fruitsLesson.exercises.deckCards[i].translatingWord}</p>
+            <p class="translating-word">${fruitsLesson.exercises.deckCards[exerciseIndex].translatingWord}</p>
         </div>
         <div class="card-back isHidden">
-            <p class="translated-word">${fruitsLesson.exercises.deckCards[i].translatedWord}</p>
+            <p class="translated-word">${fruitsLesson.exercises.deckCards[exerciseIndex].translatedWord}</p>
         </div>
     </div>`;
 
     const image = document.getElementsByClassName('card-image');
-    image.src = fruitsLesson.exercises.deckCards[i].image;
+    console.log("img " + fruitsLesson.exercises.deckCards[exerciseIndex].image);
+
+    image[0].src = fruitsLesson.exercises.deckCards[exerciseIndex].image;
 
     const card = document.getElementsByClassName('card');
     updateCardBehaviour(card);
-    return i + 1;
+    exerciseIndex++;
+
+    
+    button.classList.add('isHidden');
     
 };
 
@@ -130,14 +141,14 @@ Document.onload = function(){
 
     //progressBar
     let button = document.getElementById('complete-lesson');
-    button.addEventListener('click', progressBarBehaviour);
+    
+    button.addEventListener('click', progressBarBehaviour);//só funciona nessa ordem
     button.addEventListener('click', renderNewExercise);
-
-    let exerciseIndex = 0;
-    while(exerciseIndex < fruitsLesson.exercises.deckCards.length) {
-        console.log('jooj');
-        exerciseIndex = renderNewExercise(fruitsLesson, exerciseIndex);
-    }
+    
+    
+    
+    const card = document.getElementsByClassName('card');
+    updateCardBehaviour(card);
 
 }
 
